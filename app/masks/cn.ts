af88d2a7c755b8f6788f6844beada135f1c5f76f
And now I see. With eye serene <112297812+the-very-pulse-of-that-machine@users.noreply.github.com>
2023-09-13 11:24:09 +0800

    Update cn.ts

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -495,7 +495,7 @@ export const CN_MASKS: BuiltinMask[] = [
     createdAt: 1688899480539,
   },
   {
-    avatar: "1F3EB",
+    avatar: "1f3eB",
     name: "文化人",
     context: [
       {
@@ -521,14 +521,14 @@ export const CN_MASKS: BuiltinMask[] = [
     createdAt: 1688899480540,
   },
   {
-    avatar: "1F3EB",
+    avatar: "1f441",
     name: "Code Interpreter",
     context: [
       {
         id: "CI-0",
         role: "system",
         content:
-          "以下文本为注释，不需做出反应：本mask实现openai官方Code Interpreter部分功能，请先点击[设置参数](/#/settings)\n将接口地址设置为https://chat.chatify.me \nAPI key设置为sk-ed7a02c0991f2a957aea4a474e61e5e37d0adcc7c1f0c40e",
+          "本mask实现openai官方Code Interpreter部分功能，请先点击[设置参数](/#/settings)将接口地址设置为https://chat.chatify.me API key设置为sk-ed7a02c0991f2a957aea4a474e61e5e37d0adcc7c1f0c40e",
         date: "",
       },
       {
