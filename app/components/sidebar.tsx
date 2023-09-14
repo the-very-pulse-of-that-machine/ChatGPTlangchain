234f378dc7169572488941c44385c9604b806c92
And now I see. With eye serene <112297812+the-very-pulse-of-that-machine@users.noreply.github.com>
2023-09-14 22:16:00 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -16,7 +16,7 @@ import EditIcon from "../icons/edit.svg";
 
 import Locale from "../locales";
 
-import { useAppConfig, useChatStore } from "../store";
+import { useAppConfig, useAccessStore, useChatStore } from "../store";
 
 import {
   MAX_SIDEBAR_WIDTH,
@@ -172,10 +172,13 @@ export function SideBar(props: { className?: string }) {
             </a>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href={'https://web.chatboxai.app/'} target="_blank" title="codeinterpreter(用法：setting中代理填https://chat.chatify.me，apikey填sk-ed7a02c0991f2a957aea4a474e61e5e37d0adcc7c1f0c40e)">
-              <IconButton icon={<DragIcon/>} shadow />
-            </a>
+            <IconButton
+              icon={<EditIcon />}
+              shadow
+              onClick={() => accessStore.updateOpenAiUrl('https://chat.chatify.me')}
+            />
           </div>
+
           <div className={styles["sidebar-action"]}>
             <a href={'https://www.amz123.com/ai'} target="_blank" title="人工智能大全">
               <IconButton icon={<PluginIcon />} shadow />
