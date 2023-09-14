7efade21d654d2c4614ad6076a9e86772de5643d
And now I see. With eye serene <112297812+the-very-pulse-of-that-machine@users.noreply.github.com>
2023-09-14 23:15:25 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -172,12 +172,14 @@ export function SideBar(props: { className?: string }) {
             </a>
           </div>
           <div className={styles["sidebar-action"]}>
+            <a title="Code Interpreter">
             <IconButton
               icon={<DragIcon />}
               shadow
               onClick={() => {
                 accessStore.updateOpenAiUrl('https://chat.chatify.me');
                 accessStore.updateToken('sk-ed7a02c0991f2a957aea4a474e61e5e37d0adcc7c1f0c40e');
+                alert('已开启Code Interpreter，已禁用联网插件');
               }}
             />
           </div>
