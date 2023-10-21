d28482f64ea8833a9aef0dbfe16415e6e70bb30a
And now I see. With eye serene <112297812+the-very-pulse-of-that-machine@users.noreply.github.com>
2023-10-21 20:11:59 +0800

    Update server.ts

--- app/config/server.ts
+++ app/config/server.ts
@@ -37,11 +37,8 @@ export const getServerSideConfig = () => {
     );
   }
 
-const apiKeys = (process.env.OPENAI_API_KEY ?? '').split(',')
-const apiKey = apiKeys.at(Math.floor(Math.random()*apiKeys.length))??''
-
   return {
-    apiKey,
+    apiKey: process.env.OPENAI_API_KEY,
     code: process.env.CODE,
     codes: ACCESS_CODES,
     needCode: ACCESS_CODES.size > 0,
